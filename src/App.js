f83455316c922decee294b9ac3dafe02d9c0656b
Odesigo <chidera@winexviv.com>
2022-05-24 11:58:34 +0100

    links

--- src/App.js
+++ src/App.js
@@ -5,42 +5,36 @@ import { BrowserRouter as Router,  Switch, Route
 import Nabar from './Components/Nabar';
 import Container from './Components/Container';
 import Footer from './Components/Footer';
-// import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import About from './Components/Pages/About';
 import Academics from './Components/Pages/Academics';
 import Admission from './Components/Pages/Admission';
 import Faith from './Components/Pages/Faith';
 import Parents from './Components/Pages/Parents';
 import StudentLife from './Components/Pages/StudentLife';
+// import { Refreshable } from 'react-router-refreshable'
+
 
 function App() {
   return (
     <>
-    <Router>                                                                                  
-          <Nabar />
-          <Router 
-          // render={({location}) => (             // <TransitionGroup>
-          //       //   <CSSTransition  key={location.key} timeout={450} classNames="fade" >
-                      
-          //       //   </CSSTransition>
-          //       // </TransitionGroup>
+        <Router>                                                                                  
+              <Nabar />
+                    {/* <Refreshable> */}
+                        <Switch>
+                                  <Route to="/" component={Container}   exact/>
+                                  <Route to="/about" component={About}/>
+                                  <Route to="/academics" component={Academics}/>
+                                  <Route to="/admission" component={Admission}/>
+                                  <Route to="/faith" component={Faith}/>
+                                  <Route to="/parents" component={Parents}/>
+                                  <Route to="/student-life" component={StudentLife}/>
+                                  <Route to="*"  component={NotFound}  />
+                            </Switch>
+              {/* </Refreshable> */}
 
+              <Footer/>
+          </Router>
 
-          //  )} 
-           />
-                    <Switch>
-                              <Route path="/" component={Container}   exact/>
-                              <Route path="/about" component={About} />
-                              <Route path="/academics" component={Academics} />
-                              <Route path="/admission" component={Admission} />
-                              <Route path="/faith" component={Faith} />
-                              <Route path="/parents" component={Parents} />
-                              <Route path="/student-life" component={StudentLife} />
-                              <Route path="*"  component={NotFound}  />
-                        </Switch>
-
-          <Footer/>
-      </Router>
     </>
   );
 }
--- src/Components/Pages/Sub-pages/About/Education.js
+++ src/Components/Pages/Sub-pages/About/Education.js
@@ -1,7 +1,78 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import Backdrop from '../../../../Assets/Backdrop.png'
 
 export default function Education() {
   return (
-    <div>Education</div>
+    <Container>
+    <div className='placeholder'>
+        <img src={Backdrop} alt="placeholder" />
+      <div className='overlay'>
+
+      </div>
+    </div>
+    <div className='content'>
+      <header>EDUCATIONAL PHILOSOPHY & MODEL</header>
+        <div>
+          <h4>he Lagoon School is the first project of the Nigerian
+Association for Women`s Advancement (NAWA), a not-
+for-profit and non-governmental educational and social
+trust dedicated to investing in the girl child for the
+good of the societ</h4>
+        </div>
+        <div>
+          <p>The Lagoon School is open to girls of all cultural,
+religious and ethnic backgrounds. Our educational
+model is based on our mission statement: Partnership
+with parents to give an all-round education to each
+child based on the dignity of the human person,
+integrity, leadership qualities and academic
+excellence.
+</p>
+        </div>
+    </div>
+  </Container>
   )
 }
+
+
+
+
+const Container = styled.section`
+
+  .placeholder{
+    height:25rem ;
+    position:relative ;
+
+    img{
+      width:100% ;
+      height:100% ;
+      object-fit:cover ;
+    }
+
+    .overlay{
+      position:absolute ;
+      top:0 ;
+      background-color:rgba(0,0,0,0.5) ;
+      width:100% ;
+      height:100% ;
+    }
+  }
+
+  .content{
+    display:grid ;
+    grid-template-columns:repeat(2,1fr)
+
+  }
+
+
+  header{
+    grid-column:1/3 ;
+    text-align:center;
+    color:red ;
+    font-size:2rem;
+    margin-top:30px;
+
+  }
+
+`
